8a60986776c0156a3a53c06c856090c00f5b090b
Nikolaj Schouboe Lovring <nschouboe@gmail.com>
2024-04-19 19:08:41 +0200

    Importing global css

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,6 @@
 import type { Preview } from "@storybook/react";
 import {themeDecorator} from "./decorators/themes-decorator";
+import '../app/globals.css';
 
 const preview: Preview = {
   parameters: {
